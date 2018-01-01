e0e14485d2d6c3582abe975f965aafce61f07466
lijinwen <lijinwen@360.cn>
2018-01-01 17:27:21 +0800

    add wuenda deepleanring

---
+++ DeepLearning/wuenda/01_NeuralNetworksAndDeepLearning/week2-01.py
@@ -0,0 +1,113 @@
+# GRADED FUNCTION: sigmoid_derivative
+
+def sigmoid_derivative(x):
+    """
+    Compute the gradient (also called the slope or derivative) of the sigmoid function with respect to its input x.
+    You can store the output of the sigmoid function into variables and then use it to calculate the gradient.
+    
+    Arguments:
+    x -- A scalar or numpy array
+
+    Return:
+    ds -- Your computed gradient.
+    """
+    
+    ### START CODE HERE ### (≈ 2 lines of code)
+    s = None
+    ds = None
+    s=sigmoid(x)
+    ds = s*(1-s)
+    ### END CODE HERE ###
+    
+    return ds
+
+
+
+
+###################################
+import time
+
+x1 = [9, 2, 5, 0, 0, 7, 5, 0, 0, 0, 9, 2, 5, 0, 0]
+x2 = [9, 2, 2, 9, 0, 9, 2, 5, 0, 0, 9, 2, 5, 0, 0]
+
+### CLASSIC DOT PRODUCT OF VECTORS IMPLEMENTATION ###
+tic = time.process_time()
+dot = 0
+for i in range(len(x1)):
+    dot+= x1[i]*x2[i]
+toc = time.process_time()
+print ("dot = " + str(dot) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
+
+### CLASSIC OUTER PRODUCT IMPLEMENTATION ###
+tic = time.process_time()
+outer = np.zeros((len(x1),len(x2))) # we create a len(x1)*len(x2) matrix with only zeros
+for i in range(len(x1)):
+    for j in range(len(x2)):
+        outer[i,j] = x1[i]*x2[j]
+toc = time.process_time()
+print ("outer = " + str(outer) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
+
+### CLASSIC ELEMENTWISE IMPLEMENTATION ###
+tic = time.process_time()
+mul = np.zeros(len(x1))
+for i in range(len(x1)):
+    mul[i] = x1[i]*x2[i]
+toc = time.process_time()
+print ("elementwise multiplication = " + str(mul) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
+
+### CLASSIC GENERAL DOT PRODUCT IMPLEMENTATION ###
+W = np.random.rand(3,len(x1)) # Random 3*len(x1) numpy array
+tic = time.process_time()
+gdot = np.zeros(W.shape[0])
+for i in range(W.shape[0]):
+    for j in range(len(x1)):
+        gdot[i] += W[i,j]*x1[j]
+toc = time.process_time()
+print ("gdot = " + str(gdot) + "\n ----- Computation time = " + str(1000*(toc - tic)) + "ms")
+
+
+# GRADED FUNCTION: L1
+
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    loss = None
+    loss = sum(abs(yhat - y))
+    ### END CODE HERE ###
+    
+    return loss
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L1 = " + str(L1(yhat,y)))
+
+
+
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    loss = None
+    tmp  = yhat - y
+    loss = np.dot(tmp, tmp)
+    ### END CODE HERE ###
+    
+    return loss
+
+yhat = np.array([.9, 0.2, 0.1, .4, .9])
+y = np.array([1, 0, 0, 1, 1])
+print("L2 = " + str(L2(yhat,y)))
